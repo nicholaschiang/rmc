2a1fa18ea957516bcf60d7b166ae2dad49c42e43
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-01-25 19:08:37 -0800

    wip(storybook): get stories from `packages` dir

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../README.md',
+    '../packages/**/*.stories.mdx',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
