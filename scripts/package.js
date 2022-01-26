77f4cc1447cd813c882ca6bd5350a0248104337f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-01-25 19:08:04 -0800

    fix: add `@rmc` package name prefix

--- scripts/package.js
+++ scripts/package.js
@@ -3,14 +3,14 @@ const path = require('path');
 const example = require('./example.json');
 
 function caps(s) {
-  return s.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+  return s.replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
 }
 
 async function createPackageJSONs() {
   const dir = path.resolve(__dirname, '../packages');
   const pkgs = await fs.readdir(dir);
   for (const pkg of pkgs) {
-    example.name = pkg;
+    example.name = `@rmc/${pkg}`;
     example.description = `RMC ${caps(pkg.split('-').join(' '))} Component`;
     example.homepage = `https://github.com/nicholaschiang/rmc/tree/master/packages/${pkg}#readme`;
     const file = await fs.open(`${dir}/${pkg}/package.json`, 'w');
