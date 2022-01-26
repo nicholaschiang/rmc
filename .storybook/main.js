54b48b253a185f17594b16994563a027ae2df5d5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-01-25 18:36:28 -0800

    chore(storybook): prettify config files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,8 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+};
